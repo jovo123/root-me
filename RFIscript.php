68eb8fae1debf1dd8f0f1de3e917f20d43a95c4f
Josephine Vo <jovo123@gmail.com>
2016-02-16 12:10:32 -0800

    Delete RFIscript.php

--- RFIscript.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo "what does is mean to get a shell? open a shell remotely?"
-	echo "ok so I want to output the source code for http://challenge01.root-me.org/web-serveur/ch13/index.php through a shell on the server"
-	echo "I guess when this php script is run it should be able to make do run shell commands. I don't have to open a shell or something?"
-
-	$shell_command = "cat index.php"
-	$source = shell_exec($shell_command)
-	echo "<pre>$source</pre>"
-?>
